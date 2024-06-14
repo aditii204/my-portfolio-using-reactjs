4d489cfbbe00f4a88464f32b1844b7987d1a3da5
aditii204 <addyy204@gmail.com>
2024-06-13 21:32:31 -1200

    'update'

--- .history/src/App_20240613213142.js
+++ .history/src/App_20240613213142.js
@@ -2,7 +2,7 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
-
+import Portfolio from "./components/Portfolio";
 import Technical from "./components/Technical";
 import Contact from "./components/Contact";
 function App() {
